e414d1419e3e88fbf721f87f1f8de03908e51516
Daniel <daniel.lukins1@gmail.com>
2026-09-03 15:55:03 +1000

    finished 01_variables

--- exercises/01_variables/variables3.rs
+++ exercises/01_variables/variables3.rs
@@ -1,6 +1,9 @@
 fn main() {
     // TODO: Change the line below to fix the compiler error.
-    let x: i32;
+    let x: i32 = 69;
 
     println!("Number {x}");
+
+    let y: u8 = 9;
+    println!("Number {y}");
 }
--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -3,6 +3,14 @@ fn main() {
     println!("Spell a number: {number}");
 
     // TODO: Fix the compiler error by changing the line below without renaming the variable.
-    number = 3;
+    let number = 3;
     println!("Number plus two is: {}", number + 2);
+
+
+
+    let person: &str = "hi";
+    println!("{person}");
+
+    let person = 0;
+    println!("{person}");
 }
