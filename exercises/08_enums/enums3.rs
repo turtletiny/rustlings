df52d043d74a1c18189cf06644814063e8c03af3
Daniel <daniel.lukins1@gmail.com>
2026-09-09 12:14:47 +1000

    finished enums

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -44,8 +44,18 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: Create a match expression to process the different message
-        // variants using the methods defined above.
+        match message {
+            Message::Resize { width, height } => {
+                self.width = width;
+                self.height = height;
+            }
+            Message::Move(p) => self.position = p,
+            Message::Echo(msg) => self.message = msg,
+            Message::ChangeColor(r, g, b) => {
+                self.color = (r, g, b);
+            }
+            Message::Quit => self.quit = true,
+        }
     }
 }
 
